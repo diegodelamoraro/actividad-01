9fe2e7ab475d30caa202edd408e52404a8cd5943
diegodelamoraro <102392702+diegodelamoraro@users.noreply.github.com>
2022-08-03 21:29:54 -0500

    Create ClaseB
    Se agregó la clase B

---
+++ actividad01/actividad01/ClaseB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad01
+{
+    public class ClaseB
+    {
+        public string foo { get; set; }
+    }
+}
